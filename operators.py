678b07dc50a3a00d11f8bd53a24ce33cfea32e2f
Krutarth Patel <krutarthpatel146@gmail.com>
2023-05-30 14:06:56 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,47 @@
+# Operators
+
+# 90 + 89  ---> + - operator, 90, 89 ----> operand
+
+
+# 1. Arithmetic o/p  + - * / // **
+
+print(2 ** 3 ** 2)   # 512   RIght to left
+
+# 2. Assignment O/p   = += -= *= /= //= %= <<= >>= &= ^= |= (Low Priority)
+
+x = 89  # Assignment o/p
+
+x == 90   # Comparison O/p
+
+
+
+# -----------------------
+x = 10
+
+x += 90  # x = x + 90
+print(x)  # 100
+
+
+x -= 86  # 14
+x %= 2   # 0
+x += 300 # 300
+x /= 3   # 100.0
+x += 2   # 102.0
+
+print(x)  # 102.0
+
+x += 1
+x = x + 1
+
+# Bitwise O/p  & | ^ << >>
+
+print(45)
+
+
+
+print(34 & 920)  # 0
+print(210 | 56)
+print(1002 ^ 453)
+print(1002 << 3)
+print(908 >> 2)
+
